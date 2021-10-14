a8c05b5b4bb3242097457381a21dbf206e8176a5
GerginIvanov <gergin.ivanov@socialcloud.xyz>
2021-10-14 14:39:53 +0300

    no message

--- first_server/pm2_base_control.js
+++ first_server/pm2_base_control.js
@@ -1,8 +1,15 @@
 const pm2 = require("pm2");
+const fs = require('fs');
 
 function getBaseLogs() {
-
-    pm2.reload()
+    fs.readFile('C:\Users/Gergi/.pm2/logs/base-server-out.log', (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(data.toString());
+        return data.toString();
+    });
 }
 
-module.export = {getBaseLogs};
+
+module.exports = { getBaseLogs, };
--- first_server/server.js
+++ first_server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const pm2_base_control=require('./pm2_base_control');
+const pm2_base_control = require('./pm2_base_control');
 const pm2_second_server_control = require('../second_server/pm2_second_server_control');
 const pm2_third_server_control = require('../third_server/pm2_third_server_control');
 const app = express();
 const port = 3000;
 
+// pm2_base_control.getBaseLogs()
+// .then(result => {
+//   res.send(result);
+//   res.end();
+// });
 
-
-app.get('/logs' , (req,res) => {
-  pm2_base_control.getBaseLogs();
+app.get('/logs', (req, res) => {
+  console.log(pm2_base_control.getBaseLogs());
+  res.end();
 });
 
 
@@ -18,12 +23,12 @@ app.get('/startSecond', (req, res) => {
   res.send('Second server listening on port 3001...');
 });
 
-app.get('/stopSecond', (req,res) => {
+app.get('/stopSecond', (req, res) => {
   pm2_second_server_control.stopServerTwo();
   res.send('Second server has stopped.');
 });
 
-app.get('/restartSecond', (req,res) => {
+app.get('/restartSecond', (req, res) => {
   pm2_second_server_control.restartServerTwo();
   res.send("Server two has been restarted.");
 });
@@ -31,17 +36,17 @@ app.get('/restartSecond', (req,res) => {
 
 
 
-app.get('/startThird', (req,res) => {
+app.get('/startThird', (req, res) => {
   pm2_third_server_control.startServerThree();
   res.send('Third server listening on port 3002...');
 });
 
-app.get('/stopThird', (req,res) => {
+app.get('/stopThird', (req, res) => {
   pm2_third_server_control.stopServerThree();
   res.send('Third server has stopped.');
 });
 
-app.get('/restartThird', (req,res) => {
+app.get('/restartThird', (req, res) => {
   pm2_third_server_control.restartServerThree();
   res.send("Server three has been restarted.");
 });
