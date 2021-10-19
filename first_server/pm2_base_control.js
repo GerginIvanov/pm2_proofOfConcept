57e42874d757eceefcb5e0b8c3d7274a926aaebc
GerginIvanov <gergin.ivanov@socialcloud.xyz>
2021-10-19 14:44:51 +0300

    Currently this logs the file contents in the console every time there is a change

--- first_server/pm2_base_control.js
+++ first_server/pm2_base_control.js
@@ -1,13 +1,16 @@
 const pm2 = require("pm2");
 const fs = require('fs');
 
+
 function getBaseLogs() {
-    fs.readFile('C:\Users/Gergi/.pm2/logs/base-server-out.log', 'utf8', (err, data) => {
-        if(err){
-            console.log(err);
+    const filePath = 'C:\Users/Gergi/.pm2/logs/base-server-out.log';
+
+    //this callback is optional and only useful if there's changes to the file
+    //https://www.geeksforgeeks.org/node-js-fs-watch-method/
+    fs.watch('C:\Users/Gergi/.pm2/logs/base-server-out.log', 'utf8', (eventType, filename) => {
+        if (filename) {
+           console.log(fs.readFileSync(filePath, "utf-8"));
         }
-        console.log(data);
-        return data;
     });
 }
 
